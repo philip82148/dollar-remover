9114d7f699a78382815a66722dd352d6092973c4
philip82148 <92205270+philip82148@users.noreply.github.com>
2026-06-08 10:07:18 +0900

    implement main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import "./App.css";
+
+import { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [command, setCommand] = useState<string>("");
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <div className="w-screen h-screen flex justify-center bg-base-200">
+      <div className="container my-20 rounded-2xl bg-base-100 flex flex-col gap-10 p-10">
+        <div className="flex flex-col grow gap-2">
+          <div className="font-bold text-3xl">Input</div>
+          <textarea
+            className="textarea grow w-full"
+            placeholder="Paste Shell Prompts..."
+            onChange={async (e) => {
+              const prompt = e.target.value;
+              const cmd = prompt
+                .split("\n")
+                .map((line) =>
+                  line
+                    .trim()
+                    .replace(/^(\$|#)/, "")
+                    .trim(),
+                )
+                .join("\n");
+              setCommand(cmd);
+              await navigator.clipboard.writeText(cmd);
+              toast("Command copied to clipboard!", { type: "success" });
+            }}
+          />
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+        <div className="flex flex-col grow gap-2">
+          <div className="font-bold text-3xl">Output</div>
+          <textarea
+            className="textarea grow w-full"
+            placeholder="Commands will appear here..."
+            value={command}
+          />
         </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+      </div>
+      <ToastContainer position="bottom-center" />
+    </div>
+  );
 }
 
-export default App
+export default App;
